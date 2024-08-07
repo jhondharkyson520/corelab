b66aa786f3b7b649c77a6bd698ff2ee8ca6f47d1
jhondharkyson <jhondharkyson@gmail.com>
2024-08-07 12:56:42 -0300

    Feat: Header component

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,108 @@
+import styled from "styled-components";
+import iconSearch from '../../assets/icons/search.svg';
+import iconClose from '../../assets/icons/close.svg';
+import imgLogo from '../../assets/logo/logo.svg';
+
+const HeaderContainer = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    background-color: #FFFFFF;
+
+    height: 57px;
+    width: 100vw;
+
+    padding-left: 1rem;
+    padding-right: 1rem;
+
+    box-shadow: 0 1px 10px rgba(0, 0, 0, 0.1);
+    margin-bottom: 1rem;
+
+   
+`;
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    gap: 0.5rem;
+    padding-right: 1rem;
+    padding-bottom: 0.3rem;    
+`;
+
+const Title = styled.h1`
+    color: #455A64;
+    padding-right: 0.5rem;
+    font-weight: normal;
+    font-size: 11.45px;
+
+    @media screen and (min-width: 568px) {
+          font-size: 14px;
+
+    }
+`;
+
+const SearchContainer = styled.div`
+    width: 220px;
+    padding: 0.3rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+
+    border: 1.5px solid #D9D9D9;   
+    border-radius: 3px;
+    box-shadow: 2px 1px 5px #D9D9D9;
+    
+    @media screen and (min-width: 568px) {
+          width: 530px;         
+
+    }
+`;
+
+const SearchInput = styled.input`
+    width: 100%;
+    border: none;
+    color: #000000;
+    outline: none;
+    font-size: 10px;
+`;
+
+const SearchIcon = styled.img`
+    position: relative;
+
+    &:hover{
+        cursor: pointer;
+    }
+`;
+
+const IconClose = styled.img`
+    padding-bottom: 0.3rem;
+`;
+
+function Header(){
+    return (
+        <HeaderContainer>
+            <Container>
+                <img src={imgLogo} alt="CoreNotes Logo" />
+
+                <Title>CoreNotes</Title>
+
+                <SearchContainer>
+                    <SearchInput type="text" placeholder="Pesquisar notas" />
+                    <SearchIcon src={iconSearch} alt="Icon Search" />
+                </SearchContainer>
+
+            </Container>
+
+            <IconClose src={iconClose} alt="Icon Close" />
+        </HeaderContainer>
+    )
+}
+
+export default Header;
+
+
